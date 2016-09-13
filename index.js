7b92966ea8cc3ce6c6086d4595341a2921dbb87d
Alex Beregszaszi <alex@rtfs.hu>
2016-09-14 00:25:46 +0100

    Update API for 0xb

--- index.js
+++ index.js
@@ -4,66 +4,18 @@ const Module = Wasm.Module;
 
 // Based on sexpr-wasm-prototype/demo/demo.js
 
-function onError (loc, error, sourceLine, sourceLineColumnOffset) {
-  var lines = [
-    loc.filename + ':' + loc.line + ':' + loc.firstColumn,
-    error
-  ];
-  if (sourceLine.length > 0) {
-    var numSpaces = loc.firstColumn - 1 - sourceLineColumnOffset;
-    var numCarets =
-        Math.min(loc.lastColumn - loc.firstColumn, sourceLine.length);
-    lines.push(sourceLine);
-    lines.push(' '.repeat(numSpaces) + '^'.repeat(numCarets));
-  }
-
-  error += lines.join('\n') + '\n';
-}
-
 function compile (input) {
   var ret;
   try {
-    var allocator = wasm.LibcAllocator;
-    var eh = new wasm.SourceErrorHandler(onError, 80);
-    var buf = wasm.Buffer.fromString(input.toString());
-    var lexer = wasm.Lexer.fromBuffer(allocator, 'test.wast', buf);
-    var script = wasm.parse(lexer, eh);
-    wasm.checkAst(lexer, script, eh);
-    var memoryWriter = new wasm.MemoryWriter(allocator);
-    var jsWriter = new wasm.JSStringWriter();
-    var logStream = new wasm.Stream(jsWriter.writer);
-    var options = new wasm.WriteBinaryOptions({logStream: logStream});
-    wasm.markUsedBlocks(allocator, script);
-    wasm.writeBinaryScript(allocator, memoryWriter.base, script, options);
-    // dark magic
-    ret = new Buffer(Module.Pointer_stringify(memoryWriter.buf.buf.$addr, memoryWriter.buf.buf.$size));
+    var stackAllocator = new wasm.StackAllocator(wasm.LibcAllocator);
+    var script = wasm.parseAst(stackAllocator.allocator, 'test.wast', input);
+    script.check();
+    ret = new Buffer(script.toBinary({log: true}).buffer);
   } catch (e) {
     ret = e.toString();
   } finally {
-    if (options) {
-      options.$destroy();
-    }
-    if (logStream) {
-      logStream.$destroy();
-    }
-    if (script) {
-      script.$destroy();
-    }
-    if (jsWriter) {
-      jsWriter.$destroy();
-    }
-    if (memoryWriter) {
-      memoryWriter.$destroy();
-    }
-    if (lexer) {
-      lexer.$destroy();
-    }
-    if (buf) {
-      buf.$destroy();
-    }
-    if (eh) {
-      eh.$destroy();
-    }
+    if (script) script.$destroy();
+    if (stackAllocator) stackAllocator.$destroy();
   }
   return ret;
 }
